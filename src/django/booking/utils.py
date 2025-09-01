c9779d4d5950369b2398d237e08c659a474e5dd2
Jean28518 <jf_v@gmx.de>
2025-09-01 17:26:16 +0100

    Include parent ticket name in cancellation email subject for better context

--- src/django/booking/utils.py
+++ src/django/booking/utils.py
@@ -18,8 +18,11 @@ def send_cancel_emails(ticket, customer_timezone="Europe/Berlin", send_hint=True
     current_datetime_ticket_user = convert_time_from_utc_to_local(current_datetime, ticket.assigned_user.profile.timezone)
     current_datetime_customer = convert_time_from_utc_to_local(current_datetime, customer_timezone)
     ticket_description = booking.booking.get_ticket_description_for_customer(ticket)
+    private_ticket_name = ticket.name
+    if ticket.parent_ticket:
+        private_ticket_name = ticket.parent_ticket.name + ": " + private_ticket_name
     send_mail(
-        _("Appointment") + ' "' + ticket.name + '" ' + _("canceled."),
+        _("Appointment") + ' "' + private_ticket_name + '" ' + _("canceled."),
         _("The appointment on") + " " + current_datetime_ticket_user.strftime("%d.%m.%Y %H:%M") + ' ' + _("APPENDIX_AFTER_TIME") + ' ' + _("was canceled."),
         settings.EMAIL_HOST_USER,
         [assigned_user.email],
